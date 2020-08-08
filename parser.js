614ee394005336ff91740babf7e54dc68c56da6d
kilroyandy <kilroyandy@gmail.com>
2020-08-08 16:35:59 +0100

    Clean and Tidy
    Just neatening up the code for you, the public to have a peep at :)

---
+++ parser.js
@@ -0,0 +1,26 @@
+document.getElementById("searchValue").onclick = function getInputValue() {
+  const listOfThings = [
+    {
+      searchTerm: "string to find",
+      output: "text to display",
+      comment: "additional comment to add",
+    },
+    {
+      searchTerm: "string to find",
+      output: "text to display",
+      comment: "additional comment to add",
+    },
+  ];
+  var inputVal = document.getElementById("myInput").value;
+  var html = "";
+
+  for (i = 0; i < listOfThings.length; i++) {
+    if (inputVal.includes(listOfThings[i].searchTerm)) {
+      html += `<div><p>${listOfThings[i].output} ✅ ${listOfThings[i].comment}</p></div>`;
+    } else {
+      html += `<div><p>${listOfThings[i].output} ❌ </p></div>`;
+    }
+  }
+
+  document.querySelector("#searchResults").innerHTML = html;
+};
